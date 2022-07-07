7036742931ee06ca251ac376e385f203117af8e5
althaf18 <107619554+althaf18@users.noreply.github.com>
2022-07-07 19:14:57 +0530

    Add files via upload

---
+++ const.py
@@ -0,0 +1,7 @@
+class Student:
+    def __int__(self):
+        print("This is non-parameterised constructors")
+    def show(self,name):
+        print("Hello",name)
+Student=Student()
+Student.show("Althaf")
